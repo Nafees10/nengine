35c528f5526842e250a96c6e56a33c7af82719c4
Nafees10 <nafeeshassan16@gmail.com>
2020-06-01 04:59:49 +0500

    Removes unused arguments from isOverlapping

--- source/nengine/collision.d
+++ source/nengine/collision.d
@@ -8,8 +8,12 @@ import std.math;
 /// checks if two objects, at their current position, are overlapping.
 /// 
 /// Uses collision maps for matching
-package bool isOverlapping(int x1, int y1, int x2, int y2, GameObject object1, GameObject object2){
-	bool[uint[2]] isOccupied;
+/// 
+/// TODO: uses a janky method, come up with a better algorithm for this
+///
+/// Returns: true if objects overlapping, false if not
+package bool isOverlapping(GameObject object1, GameObject object2){
+	bool[float[2]] isOccupied;
 	foreach(y; 0 .. object1.collisionMap.height){
 		const uint cellCount = y * object1.collisionMap.width;
 		foreach (x; 0 .. object1.collisionMap.width){
@@ -28,3 +32,8 @@ package bool isOverlapping(int x1, int y1, int x2, int y2, GameObject object1, G
 	}
 	return true;
 }
+
+/// Checks if a moving object, if continues motion, will collide or not. 
+/// 
+/// Also sets moving object's position to where it should be, stopping at right before collision
+// TODO: write this
